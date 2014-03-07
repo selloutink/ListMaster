f38438773700a20f544cea06daabd53ab1f69b54
musicy <musicy404@gmail.com>
2014-03-07 21:06:40 +0100

    last

--- js/databaseacties.js
+++ js/databaseacties.js
@@ -2,13 +2,22 @@
 var db = $.db("listmaster", "1.0", "Listmaster Database", 1024 * 1024);
 
 // Maak de eerste tabel aan: Boodschappen
+if(!db.tables()){
 db.createTable({
-	name: "boodschappen",
+	name: "Alleproducten",
 	columns: 
 	[
 		"id INTEGER PRIMARY KEY",
 		"barcode INT",
-		"naam TEXT"
+		"naam TEXT",
+		"merk TEXT",
+		"beschrijving TEXT",
+		"imageurl TEXT",
+		"hoeveelheid INT",
+		"aantalkeerinlijst INT",
+		"verversing INT",
+		"totaalkeerververst INT",
+		"houdbaarheid INT"
 	],
 	done: function() {
 		console.log("Eerste tabel aangemaakt!");
@@ -17,3 +26,44 @@ db.createTable({
 		console.log("Hmm... Er is iets misgegaan...");
 	}
 });
+} else {
+console.log("Database al aangemaakt");
+}
+
+
+function createRecord(){
+db.insert("Alleproducten", {
+    data: {
+        barcode: 8722220011235,
+        naam: "Sergeant Pepper",
+		merk: "Selloutink&CO",
+		beschrijving: "Dit is een zeer mooi voorbeeldproduct",
+		imageurl: "placehold.it/100x100",
+		hoeveelheid: 1,
+		houdbaarheid: 5,
+    },
+    done: function () {
+        console.log("Yay!  I created a product!");
+    },
+    fail: function () {
+        console.log("Something went wrong....");
+    }
+});
+}
+
+
+function selectProducts(){
+	db.criteria("Alleproducten").list(
+    function (transaction, results) {
+        var rows = results.rows;
+
+        for (var i = 0; i < rows.length; i++) {
+            var row = rows.item(i);
+            console.log(row.naam + " " + row.name + " [" + row.barcode + "]");
+        }
+    },
+    function (transaction, error) {
+        console.log("Something went wrong....");
+    }
+);
+}
