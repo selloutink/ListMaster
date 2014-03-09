adc65bb4b1999d2f8eac77abb724d0198347a98d
musicy <musicy404@gmail.com>
2014-03-09 13:08:55 +0100

    commit

--- js/databaseacties.js
+++ js/databaseacties.js
@@ -30,14 +30,142 @@ db.transaction (function (transaction)
 function allItems(){
 	db.transaction (function (transaction) 
   {
-    var sql = "SELECT * FROM alleproducten"
-    transaction.executeSql (sql, undefined, function ()
+    var sql = "SELECT * FROM alleproducten ORDER BY hoeveelheid DESC"
+    transaction.executeSql (sql, undefined, function (transaction, result)
     { 
+      if (result.rows.length)
+      {
+        for (var i = 0; i < result.rows.length; i++) 
+        {
+          console.log(result.rows.item (i));
+        }
+      }
+      else
+      {
+       console.log("Geen Items");
+      }
       
     }, error);
   });
 }
 
+function allItems(){
+	db.transaction (function (transaction) 
+  {
+    var sql = "SELECT * FROM alleproducten WHERE hoeveelheid > 0"
+    transaction.executeSql (sql, undefined, function (transaction, result)
+    { 
+      if (result.rows.length)
+      {
+        for (var i = 0; i < result.rows.length; i++) 
+        {
+          console.log(result.rows.item (i));
+        }
+      }
+      else
+      {
+       console.log("Geen Items");
+      }
+      
+    }, error);
+  });
+}
+
+
+//////////////////////////////////
+// Maak lijst met items///////////
+//////////////////////////////////
+function inventoryItems(){
+	db.transaction (function (transaction) 
+  {
+    var sql = "SELECT * FROM alleproducten WHERE hoeveelheid > 0";
+    transaction.executeSql (sql, undefined, function (transaction, result)
+    { 
+      if (result.rows.length)
+      {
+		var totalhtml = "";
+        for (var i = 0; i < result.rows.length; i++) 
+        {
+		  var row = result.rows.item(i);
+          var html = '<li style="' +
+		  "background-image:url('" +
+		  row.imageurl +
+		  "'); background-size:contain; background-position:left; background-repeat:no-"+ 
+		  'repeat;">'+
+           "<div class='titel'>" +
+			   	"<span class='naam'>" +
+		  		row.naam +
+				"</span>" +
+		   "</div>" +
+           "<div class='voorraad'>" +
+		   		"<center>" +
+			   		"<span class='top'><small>Nog</small></span>" +
+					"<span class='getal'>" +
+					row.hoeveelheid +
+					"</span> " +
+					"<span class='bottom'><small>In huis</small></span>" +
+				"</center>" +
+		   "</div>" +
+           "<div class='houdbaarheid'>" +
+			   	"<center>" +
+			   		"<span class='top'><small>Houdbaar tot</small></span>" +
+					"<span class='getal'>" +
+					row.houdbaarheid +
+					"</span>" + 
+					"<span class='bottom'><small>nog " + 
+					row.houdbaarheid +
+		   " dagen</small></span>" +
+		   		"</center>" + 
+		   "</div>" +     
+           "<span class='buttonspan'>" +
+           "<div class='addonebutton'><a data-role='button' data-theme='g'>+</a></div>" +
+           "<div class='removeonebutton'><a data-role='button' data-theme='d'>-</a></div>" +
+           "</span>" +
+           "<div class='clearfix'></div>" +
+       	   "</li>";
+			totalhtml = totalhtml + html;
+			console.log(i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i)
+			console.log(row.imageurl + " url");
+			console.log(row.naam + " naam");
+			console.log(row.hoeveelheid + " hoeveelheid");
+			console.log(row.houdbaarheid + " houdbaarheid");
+        }
+		
+	  $('ul.inventorycontainer').append(totalhtml).trigger('create').listview('refresh');
+		
+      }
+      else
+      {
+       console.log("Geen Items");
+      }
+    }, error);
+  });
+}
+
+///////////////////////////////////////////
+// Voeg een product toe in je inventaris //
+///////////////////////////////////////////
+function allItems(){
+	db.transaction (function (transaction) 
+  {
+    var sql = "SELECT * FROM alleproducten WHERE hoeveelheid > 0"
+    transaction.executeSql (sql, undefined, function (transaction, result)
+    { 
+      if (result.rows.length)
+      {
+        for (var i = 0; i < result.rows.length; i++) 
+        {
+          console.log(result.rows.item (i));
+        }
+      }
+      else
+      {
+       console.log("Geen Items");
+      }
+      
+    }, error);
+  });
+}
 
 
 function error (transaction, err) 
@@ -55,13 +183,48 @@ function dropTable(sqlcode){
       console.log (" >:) Database deleted.");
     }, error);
   });
-};
+}
 
 
-function firstTimeLogin(){
+function firstTimeLogin()
+{
 		createTable();
 		// Flag the user no first time login
 		window.localStorage.setItem("loggedinbefore", true);
 		// Redirect the user to the main page
 		window.location.replace("index.html");	
 }
+
+function nieuwProduct(){
+db.transaction (function (transaction) 
+  {
+    var sql = 
+		"INSERT INTO alleproducten (" +
+		"naam ," +
+		"barcode ," +
+		"merk ," +
+		"beschrijving ," +
+		"imageurl ," +
+		"hoeveelheid, " +
+		"aantalkeerinlijst ," +
+		"verversing ," +
+		"totaalkeerververst ," +
+		"houdbaarheid)" +
+		"VALUES (" +
+		"'Mr Dirty Laundry'," +
+		"0118999881999," +
+		"'Sellout Special'," +
+		"'Sellout ink makes games about stuff' ," +
+		"'http://placehold.it/100x100' ," +
+		"0 ," +
+		"0," +
+		"5," +
+		"1," +
+		"3) ";
+	  
+    transaction.executeSql (sql, undefined, function ()
+    { 
+      console.log ("Product Toegevoegd");
+    }, error);
+  });
+};
